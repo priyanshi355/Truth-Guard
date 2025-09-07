c2caaae66491e8126af6f1521bbe70094267eae0
priyanshi355 <priyanshiyadav2505@gmail.com>
2025-09-07 16:51:26 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, render_template
+import joblib
+
+# Load model & vectorizer
+model = joblib.load("svm_model.joblib")
+vectorizer = joblib.load("tfidf_vectorizer.joblib")
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    prediction = None
+    if request.method == "POST":
+        text = request.form["text"]
+        X = vectorizer.transform([text])
+        pred = model.predict(X)[0]
+        prediction = f"Prediction: {pred}"
+    return render_template("index.html", prediction=prediction)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+
